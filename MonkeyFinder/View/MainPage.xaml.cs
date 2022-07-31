01abbf0ddcff34b5c07124e6e22064f2348779af
Ravi K R <ravi.raver@gmail.com>
2022-07-31 17:41:31 +0530

    Displaying our first monkey basic details on button click using simple lables in stack

--- MonkeyFinder/View/MainPage.xaml.cs
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -1,13 +1,24 @@
-﻿namespace MonkeyFinder.View;
+﻿using MonkeyFinder.Model;
+
+namespace MonkeyFinder.View;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	Monkey m1;
 	public MainPage()
 	{
-		InitializeComponent();
+		InitializeComponent();		
 	}
 
-}
+	private void btnShow_Clicked(object sender, EventArgs e)
+	{
+        m1 = new Monkey()
+        {
+            Name = "Baboon",
+            Details = "Africa and Asia"
+        };
 
+		lblName.Text = m1.Name;
+		lblDetails.Text = m1.Details;
+    }
+}
