a2a88c81331ccffc48f02a1882d7089f61aa609f
Ravi K R <ravi.raver@gmail.com>
2022-07-31 18:27:40 +0530

    Experimenting with databinding which we will take it up later

--- MonkeyFinder/View/MainPage.xaml.cs
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MonkeyFinder.Model;
 
+
 namespace MonkeyFinder.View;
 
 public partial class MainPage : ContentPage
@@ -7,18 +8,18 @@ public partial class MainPage : ContentPage
 	Monkey m1;
 	public MainPage()
 	{
-		InitializeComponent();		
-	}
+		InitializeComponent();
+       
+    }
 
 	private void btnShow_Clicked(object sender, EventArgs e)
 	{
-        m1 = new Monkey()
+        m1 = new Monkey
         {
             Name = "Baboon",
             Details = "Africa and Asia"
         };
 
-		lblName.Text = m1.Name;
-		lblDetails.Text = m1.Details;
+        this.BindingContext = m1;
     }
 }
